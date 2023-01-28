58ebe41b6dc930f07581e76a9163d8f53d460730
Валентина <abc99-33@yandex.ru>
2023-01-28 21:13:33 +0300

    Переход на адаптивную верстку

---
+++ script/script.js
@@ -0,0 +1,4 @@
+// let profile = document.querySelector('.profile');
+// let profileInfo = profile.querySelector('.profile__info');
+// let buttonEdit = profileInfo.querySelector('.profile__edit-button')
+// console.log(buttonEdit);@
