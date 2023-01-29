bc404bb7dc726448f5242dc5610d117ebe3edc09
Валентина <abc99-33@yandex.ru>
2023-01-29 18:28:45 +0300

    Правки ревью

--- script/index.js
+++ script/index.js
@@ -2,13 +2,13 @@ let buttonEdit = document.querySelector('.profile__button-edit');
 let popup = document.querySelector('.popup');
 let profileName = document.querySelector('.profile__name');
 let profileInfo = document.querySelector('.profile__prof');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_prof');
 
 buttonEdit.addEventListener('click', () =>{
     popup.classList.add('popup_opened');
-    let popupName = document.querySelector('.popup__name');
-    popupName.value = profileName.textContent;
-    let popupInfo = document.querySelector('.popup__info');
-    popupInfo.value = profileInfo.textContent;
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileInfo.textContent;
 });
 
 let popupClose = document.querySelector('.popup__close');
@@ -17,13 +17,12 @@ let popupClose = document.querySelector('.popup__close');
 })
 
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__info');
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
     document.querySelector('.profile__name').textContent = nameInput.value;
     document.querySelector('.profile__prof').textContent = jobInput.value;
+    popup.classList.remove('popup_opened');
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
