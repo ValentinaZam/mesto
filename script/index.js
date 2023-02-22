4a312185555fab50174e621f255a5a23ab7918bb
Валентина <abc99-33@yandex.ru>
2023-02-22 17:22:59 +0300

    Валидация форм создана

--- script/index.js
+++ script/index.js
@@ -19,7 +19,9 @@ const popupOpenImage = document.querySelector('.popup_image');
 const elementPopupImageOpen = document.querySelector('.popup__photo');
 const popupImageText = document.querySelector('.popup__text');
 const popupCloseImage = document.querySelector('.popup__close-image');
-
+const formElement = document.querySelector('.popup__form');
+const formInput = formElement.querySelector('.popup__input');
+const formError = formElement.querySelector(`.${formInput.id}-error`); 
 
 function openPopup(element) {
     element.classList.add('popup_opened');
@@ -102,3 +104,48 @@ formAddImage.addEventListener('submit', handleFormSubmitImage);
 popupCloseImage.addEventListener('click', () => {
     closePopup(popupOpenImage);
 })
+
+function showInputError(formElement, inputElement, errorMessage) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add('popup__input_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__input-error_active');
+}
+
+function hideInputError(formElement, inputElement) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove('popup__input_error');
+    errorElement.classList.remove('popup__input-error_active');
+    errorElement.textContent = '';
+}
+
+const isValid = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formElement, inputElement);
+    }
+  };
+
+  const setEventListeners = (formElement) => {
+    console.log(formElement);
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  console.log(inputList);
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        isValid(formElement, inputElement)
+      });
+    });
+  }; 
+
+  const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll('.popup__form'));
+  
+    formList.forEach((formElement) => {
+      setEventListeners(formElement);
+    });
+  };
+  
+  enableValidation(); 
+
+  
